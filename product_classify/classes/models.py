e65ed2c2984009b6c75e52fe142abeedf7b0ddf5
leffty <brigante2004@yandex.ru>
2025-06-25 13:50:01 +0300

    v1.2 - added ClassStruct model

--- product_classify/classes/models.py
+++ product_classify/classes/models.py
@@ -1,3 +1,38 @@
 from django.db import models
 
-# Create your models here.
+from .constants import (
+    CLASS_STRUCT_NAME_MAX_LENGTH,
+    CLASS_STRUCT_SHORT_NAME_MAX_LENGTH,
+)
+from ei.models import (
+    Ei,
+)
+
+
+class ClassStruct(models.Model):
+    name = models.CharField(
+        verbose_name='Название класса',
+        blank=False,
+        null=False,
+        max_length=CLASS_STRUCT_NAME_MAX_LENGTH,
+    )
+    short_name = models.CharField(
+        verbose_name='Сокращенное название класса',
+        blank=False,
+        null=False,
+        max_length=CLASS_STRUCT_SHORT_NAME_MAX_LENGTH,
+    )
+    base_ei = models.ForeignKey(
+        Ei,
+        verbose_name='Базовая единица измерения',
+        null=True,
+        blank=True,
+        on_delete=models.CASCADE,
+    )
+    main_class = models.ForeignKey(
+        'self',
+        verbose_name='Родительский класс',
+        null=True,
+        blank=False,
+        on_delete=models.CASCADE
+    )
