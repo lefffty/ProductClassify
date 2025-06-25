cb1a4e55a94e44fca5338828642e87b766368cbf
leffty <brigante2004@yandex.ru>
2025-06-25 13:19:47 +0300

    v1.0 - added Ei,Classes,Products apps, added Ei model

---
+++ product_classify/ei/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class EiConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'ei'
+    verbose_name = 'Единицы измерения'
---
+++ product_classify/ei/models.py
@@ -0,0 +1,52 @@
+from django.db import models
+
+from .constants import (
+    EI_NAME_MAX_LENGTH,
+    EI_SHORT_NAME_MAX_LENGTH,
+    EI_CODE_MAX_LENGTH
+)
+
+
+class Ei(models.Model):
+    name = models.CharField(
+        verbose_name='Название единицы измерения',
+        max_length=EI_NAME_MAX_LENGTH,
+        null=False,
+        blank=False,
+    )
+    short_name = models.CharField(
+        verbose_name='Сокращенное название единицы измерения',
+        max_length=EI_SHORT_NAME_MAX_LENGTH,
+        null=False,
+        blank=False,
+    )
+    code = models.CharField(
+        verbose_name='Код единицы измерения',
+        max_length=EI_CODE_MAX_LENGTH,
+        null=True,
+        blank=True,
+    )
+    convert_factor = models.FloatField(
+        verbose_name='Множитель единицы измерения',
+        null=True,
+        blank=True,
+    )
+    main_class = models.ForeignKey(
+        'self',
+        verbose_name='Родительский класс единицы измерения',
+        on_delete=models.DO_NOTHING,
+        null=True,
+        blank=True,
+    )
+
+    class Meta:
+        verbose_name = 'Единица измерения'
+        verbose_name_plural = 'Единицы измерения'
+
+    def __str__(self):
+        if self.short_name is not None:
+            return self.short_name
+        elif self.name is not None:
+            return self.name
+        else:
+            return 'Хуета Лев зачем то говно какое-то добавил'
