e1929f1e857b72bf4560bd6bc5959f4403930d84
leffty <brigante2004@yandex.ru>
2025-06-25 14:16:41 +0300

    v1.4 - added ProdAdmin

--- product_classify/products/admin.py
+++ product_classify/products/admin.py
@@ -1,1 +1,25 @@
 from django.contrib import admin
+
+from .models import (
+    Prod
+)
+
+
+@admin.register(Prod)
+class ProdAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'short_name',
+        'class_id',
+        'image',
+    )
+    fieldsets = (
+        (None, {
+            'fields': (
+                'name',
+                'short_name',
+                'class_id',
+                'image',
+            )
+        }),
+    )
