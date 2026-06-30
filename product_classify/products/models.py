44854a7183b6d5cef7c88d80fc25f5a5727626e4
leffty <brigante2004@yandex.ru>
2026-06-30 17:31:56 +0300

    Improve readability of ParProd __str__ and get_value

--- product_classify/products/models.py
+++ product_classify/products/models.py
@@ -89,7 +89,7 @@ class ParProd(models.Model):
         ]
 
     def clean(self):
-        cls_id = self.prod.class_field.id
+        cls_id = self.prod.class_field.pk
         class_params_ids = ParClass.objects.filter(class_field=cls_id).values_list(
             "parametr", flat=True
         )
@@ -103,31 +103,31 @@ class ParProd(models.Model):
 
         # параметр является перечислением строк
         if self.par.parametr_type.pk == ENUM_CLASSES_IDS[0] and (
-            self.enum_val.enum.main_class.pk != ENUM_CLASSES_IDS[0] or not self.enum_val
+            not self.enum_val or self.enum_val.enum.main_class.pk != ENUM_CLASSES_IDS[0]
         ):
             raise ValidationError(
                 "Для параметра типа 'Строковое перечисление' необходимо выбрать значение из списка строковых перечислений."
             )
         # параметр является перечислением изображений
         elif self.par.parametr_type.pk == ENUM_CLASSES_IDS[1] and (
-            self.enum_val.enum.main_class.pk != ENUM_CLASSES_IDS[1] or not self.enum_val
+            not self.enum_val or self.enum_val.enum.main_class.pk != ENUM_CLASSES_IDS[1]
         ):
             raise ValidationError(
                 "Для параметра типа 'Перечисление изображений' необходимо выбрать значение из списка перечислений изображений."
             )
         # параметр является целочисленным перечислением
         elif self.par.parametr_type.pk == ENUM_CLASSES_IDS[2] and (
-            self.enum_val.enum.main_class.pk != ENUM_CLASSES_IDS[2] or not self.enum_val
-        ):
-            raise ValidationError(
-                "Для параметра типа 'Целочисленное перечисление' необходимо выбрать значение из списка целочисленных перечислений."
-            )
-        # параметр является вещественным перечислением
-        elif self.par.parametr_type.pk == ENUM_CLASSES_IDS[3] and (
-            self.enum_val.enum.main_class.pk != ENUM_CLASSES_IDS[3] or not self.enum_val
+            not self.enum_val or self.enum_val.enum.main_class.pk != ENUM_CLASSES_IDS[2]
         ):
             raise ValidationError(
                 "Для параметра типа 'Вещественное перечисление' необходимо выбрать значение из списка вещественных перечислений."
+            )
+        # параметр является вещественным перечислением
+        elif self.par.parametr_type.pk == ENUM_CLASSES_IDS[3] and (
+            not self.enum_val or self.enum_val.enum.main_class.pk != ENUM_CLASSES_IDS[3]
+        ):
+            raise ValidationError(
+                "Для параметра типа 'Целочисленное перечисление' необходимо выбрать значение из списка целочисленных перечислений."
             )
         # параметр является целочисленным
         elif self.par.parametr_type.pk == INT_PARAMS and not self.int_value:
@@ -140,60 +140,44 @@ class ParProd(models.Model):
                 "Для параметра типа 'Вещественное число' необходимо указать вещественное значение."
             )
 
-    def __str__(self):
-        # параметр является перечислением
-        if self.enum_val:
-            # перечисление строк
-            if self.enum_val.enum.main_class.id == ENUM_CLASSES_IDS[0]:
-                return self.prod.name + " - " + self.enum_val.name
-            # перечисление изображений
-            elif self.enum_val.enum.main_class.id == ENUM_CLASSES_IDS[1]:
-                return self.prod.name + " - " + self.enum_val.short_name
-            # вещественное перечисление
-            elif self.enum_val.enum.main_class.id == ENUM_CLASSES_IDS[2]:
-                return (
-                    self.prod.name
-                    + " - "
-                    + self.enum_val.short_name
-                    + " - "
-                    + str(self.enum_val.double_value)
-                )
-            # целочисленное перечисление
-            else:
-                return (
-                    self.prod.name
-                    + " - "
-                    + self.enum_val.short_name
-                    + " - "
-                    + str(self.enum_val.int_value)
-                )
-        # параметр является целочисленным
-        elif self.int_value:
-            return self.prod.name + " - " + self.par.name + " - " + str(self.int_value)
-        # параметр является вещественным
+    def _get_enum_display_value(self):
+        """Возвращает строковое представление для значения перечисления в зависимости от его типа."""
+        enum_type_id = self.enum_val.enum.main_class.id
+        if enum_type_id == ENUM_CLASSES_IDS[0]:  # строковое
+            return self.enum_val.name
+        elif enum_type_id == ENUM_CLASSES_IDS[1]:  # изображение
+            return self.enum_val.short_name
+        elif enum_type_id == ENUM_CLASSES_IDS[2]:  # вещественное
+            return f"{self.enum_val.short_name} - {self.enum_val.double_value}"
+        else:  # целочисленное
+            return f"{self.enum_val.short_name} - {self.enum_val.int_value}"
+
+    def _get_enum_raw_value(self):
+        """Возвращает сырое значение перечисления (для get_value)."""
+        enum_type_id = self.enum_val.enum.main_class.id
+        if enum_type_id == ENUM_CLASSES_IDS[0]:
+            return self.enum_val.name
+        elif enum_type_id == ENUM_CLASSES_IDS[1]:
+            return self.enum_val.image
+        elif enum_type_id == ENUM_CLASSES_IDS[2]:
+            return self.enum_val.double_value
         else:
-            return (
-                self.prod.name + " - " + self.par.name + " - " + str(self.double_value)
-            )
+            return self.enum_val.int_value
+
+    def __str__(self):
+        if self.enum_val:
+            return f"{self.prod.name} - {self._get_enum_display_value()}"
+        elif self.int_value is not None:  # явная проверка на None
+            return f"{self.prod.name} - {self.par.name} - {self.int_value}"
+        else:
+            return f"{self.prod.name} - {self.par.name} - {self.double_value}"
 
     def get_value(self):
-        # параметр является целочисленным
-        if self.int_value:
+        if self.int_value is not None:
             return self.int_value
-        # параметр является вещественным
-        elif self.double_value:
+        elif self.double_value is not None:
             return self.double_value
-        # параметр является перечислением
+        elif self.enum_val:
+            return self._get_enum_raw_value()
         else:
-            # перечисление строк
-            if self.enum_val.enum.main_class.id == ENUM_CLASSES_IDS[0]:
-                return self.enum_val.name
-            # перечисление изображений
-            elif self.enum_val.enum.main_class.id == ENUM_CLASSES_IDS[1]:
-                return self.enum_val.image.instance
-            # вещественное перечисление
-            elif self.enum_val.enum.main_class.id == ENUM_CLASSES_IDS[2]:
-                return self.enum_val.double_value
-            # целочисленное перечисление
-            else:
-                return self.enum_val.int_value
+            return None  # или выбросить исключение, если недопустимо
