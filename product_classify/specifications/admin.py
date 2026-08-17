2c4db54b3b46a912f195cf2075d13a938ac22be1
leffty <brigante2004@yandex.ru>
2026-08-17 17:24:57 +0300

    Add ProdComponent and SpecificationLogs model into admin-zone

--- product_classify/specifications/admin.py
+++ product_classify/specifications/admin.py
@@ -1,1 +1,53 @@
 from django.contrib import admin
+
+from specifications.models import ProdComponent, SpecificationLogs
+
+
+@admin.register(ProdComponent)
+class ProdComponentAdmin(admin.ModelAdmin):
+    list_display = (
+        "pk",
+        "parent_prod",
+        "component",
+        "num",
+        "quantity",
+    )
+    fieldsets = (
+        (
+            None,
+            {
+                "fields": (
+                    "parent_prod",
+                    "component",
+                    "num",
+                    "quantity",
+                )
+            }
+        ),
+    )
+
+
+@admin.register(SpecificationLogs)
+class SpecificationLogsAdmin(admin.ModelAdmin):
+    list_display = (
+        "pk",
+        "pair",
+        "updated_at",
+        "old_quantity",
+        "new_quantity",
+    )
+    fieldsets = (
+        (
+            None,
+            {
+                "fields": (
+                    "pair",
+                    "old_quantity",
+                    "new_quantity",
+                ),
+            }
+        ),
+    )
+    readonly_fields = (
+        "updated_at",
+    )
