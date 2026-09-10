1e9575f27c14b1f91ebd00a4c18f756059227367
leffty <brigante2004@yandex.ru>
2026-09-10 11:01:58 +0300

    feat: add model GroupWorkingCenter to admin-zone

--- product_classify/route_tech/admin.py
+++ product_classify/route_tech/admin.py
@@ -2,6 +2,7 @@ from django.contrib.admin import ModelAdmin, register
 
 from route_tech.models import (
     EconomicActivitySubject,
+    GroupWorkingCenter,
 )
 
 
@@ -22,6 +23,31 @@ class EconomicActivitySubjectAdmin(ModelAdmin):
                     "short_name",
                     "main_class",
                     "main_subject",
+                ),
+            },
+        ),
+    )
+
+
+@register(GroupWorkingCenter)
+class GroupWorkingCenterAdmin(ModelAdmin):
+    list_display = (
+        "name",
+        "short_name",
+        "main_class",
+        "eas",
+        "place",
+    )
+    fieldsets = (
+        (
+            None,
+            {
+                "fields": (
+                    "name",
+                    "short_name",
+                    "main_class",
+                    "eas",
+                    "place",
                 ),
             },
         ),
